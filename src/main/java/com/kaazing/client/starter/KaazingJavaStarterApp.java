203c9f41dc087a139916799e583be1eb36add8ad
Roman Smolgovsky <roman@nemiga.net>
2016-03-28 13:31:03 -0700

    Updated indentation.

--- src/main/java/com/kaazing/client/starter/KaazingJavaStarterApp.java
+++ src/main/java/com/kaazing/client/starter/KaazingJavaStarterApp.java
@@ -19,63 +19,66 @@ import com.kaazing.client.universal.UniversalClientProtocol;
 /**
  * Simple starter application to illustrate how to communicate with Kaazing Web
  * Gateway
- * 
- * @author romans
  *
  */
 public class KaazingJavaStarterApp {
-	public static void main(String[] args) throws Exception {
-		try (UniversalClient universalClient = UniversalClientFactory.createUniversalClient(UniversalClientProtocol.amqp, // Use AMQP
-				new URI("ws://localhost:8001/amqp"), // Kaazing Gateway URL
-				"guest", // Login to use to connect to Kaazing Gateway
-				"guest", // Password to use to connect to Kaazing Gateway
-				new ErrorsListener() { // Error listener callback - simply print errors
-					@Override
-					public void onException(ClientException exception) {
-						System.err.println("Exception occurred! " + exception.getMessage());
-					}
-				});) {
+    public static void main(String[] args) throws Exception {
+        try (UniversalClient universalClient = UniversalClientFactory.createUniversalClient(UniversalClientProtocol.amqp, // Use
+                                                                                                                          // AMQP
+                new URI("ws://localhost:8001/amqp"), // Kaazing Gateway URL
+                "guest", // Login to use to connect to Kaazing Gateway
+                "guest", // Password to use to connect to Kaazing Gateway
+                new ErrorsListener() { // Error listener callback - simply print
+                                       // errors
+                    @Override
+                    public void onException(ClientException exception) {
+                        System.err.println("Exception occurred! " + exception.getMessage());
+                    }
+                });) {
 
-			ClientSubscription subscription = universalClient.subscribe("test", // publishing point
-					"test", // subscription point
-					new MessagesListener() { // Message listener - simply print messages
-						@Override
-						public void onMessage(Serializable message) {
-							System.out.println("Received message: " + message.toString());
-						}
-					}, false); // We want to receive our own messages - noLocal is false
+            ClientSubscription subscription = universalClient.subscribe("test", // publishing
+                                                                                // point
+                    "test", // subscription point
+                    new MessagesListener() { // Message listener - simply print
+                                             // messages
+                        @Override
+                        public void onMessage(Serializable message) {
+                            System.out.println("Received message: " + message.toString());
+                        }
+                    }, false); // We want to receive our own messages - noLocal
+                               // is false
 
-			System.out.println("Kaazing Java Starter App. Copyright (C) 2016 Kaazing, Inc.");
-			System.out.println("Type the message to send or <exit> to stop.");
-			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-			while (true) {
-				String text = console.readLine();
-				if (text.toLowerCase().equals("<exit>"))
-					break;
-				// Send as a text
-				subscription.sendMessage(text);
-				// Send as an object
-				subscription.sendMessage(new TestObjectMessage(text));
-			}
-		}
-	}
+            System.out.println("Kaazing Java Starter App. Copyright (C) 2016 Kaazing, Inc.");
+            System.out.println("Type the message to send or <exit> to stop.");
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String text = console.readLine();
+                if (text.toLowerCase().equals("<exit>"))
+                    break;
+                // Send as a text
+                subscription.sendMessage(text);
+                // Send as an object
+                subscription.sendMessage(new TestObjectMessage(text));
+            }
+        }
+    }
 
-	public static class TestObjectMessage implements Serializable {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1721747391179889337L;
-		private String message;
+    public static class TestObjectMessage implements Serializable {
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 1721747391179889337L;
+        private String message;
 
-		public TestObjectMessage(String message) {
-			this.message = message;
-		}
+        public TestObjectMessage(String message) {
+            this.message = message;
+        }
 
-		@Override
-		public String toString() {
-			return "TestObjectMessage[" + this.message + "]";
-		}
+        @Override
+        public String toString() {
+            return "TestObjectMessage[" + this.message + "]";
+        }
 
-	}
+    }
 
 }
